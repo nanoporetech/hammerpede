19c4d63aad8dbae7decb0e4be278d9d283234ff9
Botond Sipos <Botond.Sipos@nanoporetech.com>
2020-12-08 10:46:54 +0000

    spoa fasta fix.

--- hammerpede/spoa.py
+++ hammerpede/spoa.py
@@ -4,16 +4,6 @@ import os
 
 
 def spoa_align(inf, outf):
-    tmpf = inf + ".SPOA_TMP"
-    cmd = "spoa -l 1 -r 1 {} > {}".format(inf, tmpf)
+    cmd = "spoa -l 1 -r 1 {} > {}".format(inf, outf)
     sp.check_call(cmd, shell=True)
-    ofh = open(outf, "w")
-    count = 0
-    with open(tmpf, "r") as fh:
-        fh.readline()
-        for line in fh:
-            ofh.write(">s{}\n{}".format(count, line))
-    ofh.flush()
-    ofh.close()
-    os.unlink(tmpf)
     return outf
